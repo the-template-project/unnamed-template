8895ae33438e74139ac6b7421616506d610e365a
Alexander Hristov <xerauquil@gmail.com>
2020-06-14 03:22:56 +0300

    Created the About page. Minified images. Will open issues  "add a 'fade in' to all pages" and "use the useDimensions hook in Home.tsx".

---
+++ src/useDimensions.ts
@@ -0,0 +1,16 @@
+import React, { useLayoutEffect } from 'react';
+
+
+const useDimensions = <T extends HTMLElement>(
+  el: React.RefObject<T>,
+  callBack: (rect: DOMRectReadOnly) => void,
+): void => {
+  useLayoutEffect(() => {
+    const observer: ResizeObserver = new ResizeObserver(([element]) => {
+      callBack(element.contentRect);
+    });
+    observer.observe(el.current!);
+  }, []);
+};
+
+export default useDimensions;
